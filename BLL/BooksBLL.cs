66608fded73515509443fad33f2e95fdf4ca6409
Levid <jassontejada@gmail.com>
2023-02-13 18:49:54 -0400

    Agregando la BLL

---
+++ BLL/BooksBLL.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+
+public class BooksBLL
+{
+    private Context context;
+
+    public BooksBLL(Context context)
+    {
+        this.context = context;
+    }
+
+    public bool Exist(int BookId)
+    {
+        return context.Book.Any(book => book.BookId == BookId);
+    }
+
+    public bool Insert(Books Book)
+    {
+        context.Book.Add(Book);
+        return context.SaveChanges() > 0;
+    }
+
+    public bool Modify(Books Book)
+    {
+        context.Entry(Book).State = EntityState.Modified;
+        return context.SaveChanges() > 0;
+    }
+
+    public bool Save(Books Book)
+    {
+        if(Exist(Book.BookId))
+            return Insert(Book);
+        else
+            return Modify(Book);
+    }
+
+    public Books? Search(int BookId)
+    {
+        return context.Book.Where(book => book.BookId == BookId).AsNoTracking().SingleOrDefault();
+    }
+
+}
