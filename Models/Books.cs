be2e2647948c15734f2c63bbc81fd82affa62999
Levid <jassontejada@gmail.com>
2023-02-13 19:40:43 -0400

    Agregando Required MSG

--- Models/Books.cs
+++ Models/Books.cs
@@ -4,6 +4,8 @@ public class Books
 {
     [Key]
     public int BookId { get; set; }
+    [Required(ErrorMessage ="El título es necesario.")]
     public string? Title { get; set; }
+    [Required(ErrorMessage ="El precio es necesario.")]
     public float Price { get; set; }
 }
